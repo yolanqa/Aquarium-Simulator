25c1c7d8abb164aeb1c2481063b1a98bc4c12070
yolanqa <yolanda.stefanescu@yahoo.com>
2026-03-06 01:33:03 +0200

    am adaugat 4 clase, constructori, operatori

--- main.cpp
+++ main.cpp
@@ -1,8 +1,154 @@
 #include <iostream>
 #include <array>
+#include <cstring>
+#include <ostream>
+#include <vector>
 #include "include/Example.h"
 // This also works if you do not want `include/`, but some editors might not like it
 // #include "Example.h"
+
+
+class Hrana {
+private:
+    std::string tip;
+    double cantitate;
+    double valoare_nutritiva;
+public:
+    //Hrana(const std::string &tip, double cantitate, double valoare_nutritiva);
+
+    Hrana(const std::string &tip, const double cantitate, const double valoare_nutritiva)
+        : tip(tip),
+          cantitate(cantitate),
+          valoare_nutritiva(valoare_nutritiva) {
+    }
+
+    Hrana(const Hrana &other)
+        : tip(other.tip),
+          cantitate(other.cantitate),
+          valoare_nutritiva(other.valoare_nutritiva) {
+    }
+
+    Hrana & operator=(const Hrana &other) {
+        if (this == &other)
+            return *this;
+        tip = other.tip;
+        cantitate = other.cantitate;
+        valoare_nutritiva = other.valoare_nutritiva;
+        return *this;
+    }
+
+    ~Hrana() = default;
+
+    friend std::ostream & operator<<(std::ostream &os, const Hrana &obj) {
+        return os
+               << "tip: " << obj.tip
+               << " cantitate: " << obj.cantitate
+               << " valoare_nutritiva: " << obj.valoare_nutritiva;
+    }
+};
+
+class Animal {
+private:
+    char* nume;
+    std::string specie;
+    double greutate;
+    int varsta;
+    Hrana hrana_preferata;
+
+
+    void functie(const char* src) {
+        if (src) {
+            nume= new char[strlen(src) + 1];
+            strcpy(nume,src);
+        }else {
+            nume= nullptr;
+        }
+    }
+public:
+    //Animal(const char* nume, const std::string& specie, double greutate, int varsta, const Hrana& hrana_preferata);
+
+    Animal(const char* nume, const std::string &specie, double greutate, int varsta,
+        const Hrana &hrana_preferata)
+        : specie(specie),
+          greutate(greutate),
+          varsta(varsta),
+          hrana_preferata(hrana_preferata) { functie(nume); }
+
+
+    Animal(const Animal &other)
+        :
+          specie(other.specie),
+          greutate(other.greutate),
+          varsta(other.varsta),
+          hrana_preferata(other.hrana_preferata) { functie(other.nume);}
+
+
+    Animal & operator=(const Animal &other) {
+        if (this == &other)
+            return *this;
+        delete[] nume;
+        functie(other.nume);
+        specie = other.specie;
+        greutate = other.greutate;
+        varsta = other.varsta;
+        hrana_preferata = other.hrana_preferata;
+        return *this;
+    }
+
+    ~Animal() {
+        delete[] nume;
+    }
+
+
+    friend std::ostream & operator<<(std::ostream &os, const Animal &obj) {
+        return os
+               << "nume: " << (obj.nume ? obj.nume : "(null)")
+               << " specie: " << obj.specie
+               << " greutate: " << obj.greutate
+               << " varsta: " << obj.varsta
+               << " hrana_preferata: " << obj.hrana_preferata;
+    }
+};
+
+class Bazin {
+private:
+    std::vector<Animal> animale;
+    int volum;
+    double temperatura;
+    int salinitate;
+
+public:
+    //Bazin(int volum, double temperatura, int salinitate);
+
+
+    Bazin(const std::vector<Animal> &animale, const int volum, const double temperatura, const int salinitate)
+        : animale(animale),
+          volum(volum),
+          temperatura(temperatura),
+          salinitate(salinitate) {
+    }
+
+    ~Bazin() = default;
+};
+
+class Acvariu{
+private:
+    std::vector<Bazin> bazine;
+    std::string nume;
+    double buget;
+
+public:
+    //Acvariu(const std::string& nume, double buget);
+
+    Acvariu(const std::vector<Bazin> &bazine, const std::string &nume, const double buget)
+        : bazine(bazine),
+          nume(nume),
+          buget(buget) {
+    }
+
+    ~Acvariu() = default;
+};
+
 
 int main() {
     std::cout << "Hello, world!\n";
